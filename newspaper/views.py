7eae2ca52070f1fe25956c23710b6c00c4979556
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-06 22:25:48 +0200

    feat: Add views for News Redactor models

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from newspaper.forms import NewsForm, RedactorCreationForm
+from newspaper.models import News
+
+
+class NewsListView(generic.ListView):
+    model = News
+
+
+class NewsDetailView(generic.DetailView):
+    model = News
+
+
+class NewsCreateView(generic.CreateView):
+    model = News
+    form_class = NewsForm
+    success_url = reverse_lazy("newspaper:news-list")
+
+
+class NewsUpdateView(generic.UpdateView):
+    model = News
+    form_class = NewsForm
+    success_url = reverse_lazy("newspaper:news-list")
+
+
+class NewsDeleteView(generic.DeleteView):
+    model = News
+    success_url = reverse_lazy("newspaper:news-list")
+
+
+class RedactorListView(generic.ListView):
+    model = News
+
+
+class RedactorDetailView(generic.DetailView):
+    model = News
+
+
+class RedactorCreateView(generic.CreateView):
+    model = News
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorUpdateView(generic.UpdateView):
+    model = News
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorDeleteView(generic.DeleteView):
+    model = News
+    success_url = reverse_lazy("newspaper:news-list")
