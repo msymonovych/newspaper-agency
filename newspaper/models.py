9b724da1571c82235f12985c3fd2404277d69e40
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-06 15:06:54 +0200

    add Topic News Redactor models

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ManyToManyField(Topic, related_name="news")
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="news"
+    )
+
+    def __str__(self):
+        return f"{self.title} {self.publish_date}"
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return self.username
