37a4a630f81f3cfd2095d1bbefd56bbc695d59f5
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-06 17:09:04 +0200

    feat: Register models in admin

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newspaper.models import Topic, News, Redactor
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional Information", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                 "Additional Information",
+                 {
+                     "fields": (
+                         "first_name",
+                         "last_name",
+                         "years_of_experience",
+                     )
+                 },
+            ),
+        )
+    )
+
+
+admin.site.register(Topic)
+admin.site.register(News)
