881244c0f1595b8cb94fe6e2c006ed698376dab2
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-08 13:51:30 +0200

    chore: reduce pagination number to 5

--- newspaper/views.py
+++ newspaper/views.py
@@ -13,7 +13,7 @@ from newspaper.models import News
 
 
 class NewsListView(generic.ListView):
-    paginate_by = 10
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
