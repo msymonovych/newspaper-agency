8811076c9207607bf08af5a362f2fa74b50e1309
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-08 13:33:33 +0200

    refactor: Update settings.py with static file settigs

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -118,9 +118,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "newspaper.Redactor"
+
+LOGIN_REDIRECT_URL = "/"
