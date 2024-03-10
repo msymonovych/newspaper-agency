2d1c196a03911343f8ede34c59609568d8c5ed97
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-10 19:17:08 +0200

    test: Add tests to newspaper app

---
+++ newspaper/tests/test_forms.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from newspaper.forms import RedactorCreationForm
+
+
+class FormsTest(TestCase):
+    def test_redactor_creation_form(self):
+        test_password = "testPass1"
+        form_data = {
+            "username": "test_user",
+            "password1": test_password,
+            "password2": test_password,
+            "first_name": "User",
+            "last_name": "Test",
+            "years_of_experience": 10
+        }
+        form = RedactorCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
---
+++ newspaper/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from newspaper.models import Topic, Redactor, News
+
+
+class ModelsTest(TestCase):
+    def setUp(self):
+        self.topic = Topic.objects.create(name="testTopic")
+        self.redactor = Redactor.objects.create(
+            username="testRedactor",
+            password="testPass1",
+            first_name="testFirstName",
+            last_name="testLastName",
+            years_of_experience=9
+        )
+        self.newspaper = News.objects.create(
+            title="testNewspaper",
+            content="testContent",
+        )
+
+    def test_topic_str(self):
+        self.assertEqual(str(self.topic), "testTopic")
+
+    def test_redactor_str(self):
+        self.assertEqual(str(self.redactor), "testRedactor")
+
+    def test_newspaper_str(self):
+        self.assertEqual(
+            str(self.newspaper),
+            f"{self.newspaper.title} {self.newspaper.publish_date}"
+        )
