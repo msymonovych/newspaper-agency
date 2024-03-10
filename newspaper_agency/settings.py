b9e618a72e1008a4debc5d6640917f9cd472be45
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-10 17:19:46 +0200

    chore: Add crispy to INSTALLED_APPS in settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "newspaper",
+    "crispy_forms",
+    "crispy_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "newspaper.Redactor"
 
 LOGIN_REDIRECT_URL = "/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
