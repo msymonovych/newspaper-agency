d5b968ea55ed932c6b230a19a12669c811821091
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-10 15:05:46 +0200

    refactor: Make 'name' field unique in Topic table

--- newspaper/models.py
+++ newspaper/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
